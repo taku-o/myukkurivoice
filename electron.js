075f0c54fb3ccfdd02ffe434b04fb330c92f8887
taku.omi <taku.omi@gree.co.jp>
2016-12-21 16:13:06 +0900

    add initial menu.

--- electron.js
+++ electron.js
@@ -1,10 +1,11 @@
 'use strict';
 const electron = require('electron');
-const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const app = electron.app;
 const dialog = electron.dialog;
-const ipcMain = electron.ipcMain;
 const globalShortcut = electron.globalShortcut;
+const ipcMain = electron.ipcMain;
+const Menu = electron.Menu;
 
 // global reference
 var mainWindow = null;
@@ -13,9 +14,9 @@ var mainWindow = null;
 app.on('window-all-closed', function() {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform != 'darwin') {
+  //if (process.platform != 'darwin') {
     app.quit();
-  }
+  //}
 });
 
 // This method will be called when Electron has finished
@@ -38,13 +39,11 @@ app.on('ready', function() {
   mainWindow.on('closed', function() {
     // dereference global reference
     mainWindow = null;
-    // quit
-    app.quit();
   });
 
   // shortcut
   var r = globalShortcut.register('Command+Q', function() {
-	app.quit();
+    app.quit();
   });
   var r = globalShortcut.register('Command+P', function() {
     if (mainWindow) { mainWindow.webContents.send('shortcut', 'play'); }
@@ -62,6 +61,110 @@ app.on('ready', function() {
     if (mainWindow) { mainWindow.webContents.send('shortcut', 'encode'); }
   });
 
+  // menu
+  var menu_list = [
+    {
+      label: 'MYukkuriVoice',
+      submenu: [
+        { role: 'about' },
+        { type: 'separator' },
+        {
+          role: 'services',
+          submenu: []
+        },
+        { type: 'separator' },
+        {
+          role:'quit',
+          accelerator: 'Command+Q'
+        }
+      ]
+    },
+    {
+      label: '編集',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        { role: 'pasteandmatchstyle' },
+        { role: 'delete' },
+        { role: 'selectall' },
+        { type: 'separator' },
+        { label: 'Speech',
+          submenu: [
+            { role: 'startspeaking' },
+            { role: 'stopspeaking' }
+          ]
+        }
+      ]
+    },
+    {
+      label: 'ボイス',
+      submenu: [
+        {
+          label:'音記号列に変換',
+          accelerator: 'Command+Right'
+        },
+        { type: 'separator' },
+        {
+          label:'再生',
+          accelerator: 'Command+P'
+        },
+        {
+          label:'記録',
+          accelerator: 'Command+S'
+        }
+      ]
+    },
+    {
+      label: '表示',
+      submenu: [
+        { role: 'reload' },
+        { role: 'toggledevtools' },
+        { type: 'separator' },
+        { role: 'resetzoom' },
+        { role: 'zoomin' },
+        { role: 'zoomout' },
+        { type: 'separator' },
+        { role: 'togglefullscreen' }
+      ]
+    },
+    {
+      label: 'ウインドウ',
+      submenu: [
+        {
+          label: 'Minimize',
+          accelerator: 'CmdOrCtrl+M',
+          role: 'minimize'
+        },
+        {
+          label: 'Zoom',
+          role: 'zoom'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Bring All to Front',
+          role: 'front'
+        }
+      ]
+    },
+    {
+      label: 'ヘルプ',
+      submenu: [
+        {
+          label: 'Learn More',
+          click () { require('electron').shell.openExternal('https://github.com/taku-o/myukkurivoice') }
+        }
+      ]
+    }
+  ];
+  var menu_template = Menu.buildFromTemplate(menu_list);
+  Menu.setApplicationMenu(menu_template);
+
   // Open the DevTools.
   // mainWindow.openDevTools();
 });
@@ -77,3 +180,4 @@ ipcMain.on('showSaveDialog', function (event, message) {
   var r = dialog.showSaveDialog(mainWindow, options);
   event.sender.send('showSaveDialog', r);
 })
+
