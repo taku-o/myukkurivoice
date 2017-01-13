fee53cd85a7079e2eee6002f8a9763705c788351
taku.omi <taku.omi@gree.co.jp>
2017-01-13 10:39:57 +0900

    add build shell script for mac app store.

---
+++ bin/maspkg.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+productbuild --component "myukkurivoice-mas-x64/myukkurivoice.app" /Applications --sign "3rd Party Mac Developer Installer: Taku Omi (52QJ97GWTE)" "myukkurivoice-mas-x64/myukkurivoice.pkg"
