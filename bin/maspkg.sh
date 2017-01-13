12d3afe945e4eeb5fb09bb933ae122a3ca277cb5
taku.omi <taku.omi@gree.co.jp>
2017-01-13 10:38:20 +0900

    remove unneeded shell.

--- bin/maspkg.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-productbuild --component "myukkurivoice-mas-x64/myukkurivoice.app" /Applications --sign "3rd Party Mac Developer Installer: Taku Omi (52QJ97GWTE)" "myukkurivoice-mas-x64/myukkurivoice.pkg"
