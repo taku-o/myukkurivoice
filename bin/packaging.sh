5927712aef4ad89bf672402b3db3f45313a130d2
taku-o <mail@nanasi.jp>
2017-03-18 20:24:21 +0900

    add packaging command.

--- bin/packaging.sh
+++ bin/packaging.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+rm -rf myukkurivoice-darwin-x64/
+rm -f myukkurivoice-darwin-x64.zip
 electron-packager . myukkurivoice --platform=darwin --arch=x64 --version=1.4.12 --icon=icns/myukkurivoice.icns --overwrite --ignore="(\.gitignore|\.gitmodules|docs|icns|README.md|vendor/aqk2k_mac|vendor/aqtk1-mac|vendor/aqtk2-mac)" --asar.unpackDir=vendor
 
