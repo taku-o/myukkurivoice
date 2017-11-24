1ae8f860f6bf91e7fb102cac4a0ca79c192a4518
taku.omi <taku.omi@gree.co.jp>
2017-11-24 19:51:53 +0900

    add test code.

--- bin/test.sh
+++ bin/test.sh
@@ -1,4 +1,11 @@
 cd `dirname $0`
 cd ..
 
+# build package
+if [ ! -d MYukkuriVoice-darwin-x64 ]
+then
+    electron-packager . MYukkuriVoice --platform=darwin --arch=x64 --electronVersion=1.7.9 --icon=icns/myukkurivoice.icns --overwrite --ignore="(\.gitignore|\.gitmodules|docs|icns|test|README.md|vendor/aqk2k_mac|vendor/aqtk1-mac|vendor/aqtk2-mac)" --asar.unpackDir=vendor
+fi
+
+# run test
 NODE_PATH="$HOME/.nvm/versions/node/v8.2.1/lib/node_modules" mocha
--- test/test.js
+++ test/test.js
@@ -1,26 +1,36 @@
 var Application = require('spectron').Application
 var assert = require('assert')
 
-describe('application launch', function () {
+describe('application launch', function() {
   this.timeout(10000)
 
-  beforeEach(function () {
+  beforeEach(function() {
     this.app = new Application({
       path: 'MYukkuriVoice-darwin-x64/MYukkuriVoice.app/Contents/MacOS/MYukkuriVoice'
     })
     return this.app.start()
-  })
+  });
 
-  afterEach(function () {
+  afterEach(function() {
     if (this.app && this.app.isRunning()) {
       return this.app.stop()
     }
-  })
+  });
 
-  it('shows an initial window', function () {
+  // test
+  it('open application window at startup', function() {
     return this.app.client.getWindowCount().then(function (count) {
       assert.equal(count, 1)
     })
-  })
-})
+  });
+
+  // menu
+  it('menu', function(done) {
+    //var menu = this.app.electron.remote.Menu.getApplicationMenu();
+    //assert.ok(true);
+    return done();
+  });
+
+
+});
 
