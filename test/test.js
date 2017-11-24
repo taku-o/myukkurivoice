0a0da598760c6c0082828b07b0940c64a1c31be6
taku-o <mail@nanasi.jp>
2017-11-25 06:13:56 +0900

    update test code.

--- test/test.js
+++ test/test.js
@@ -1,36 +1,173 @@
-var Application = require('spectron').Application
-var assert = require('assert')
+var Application = require('spectron').Application;
+var assert = require('assert');
 
 describe('application launch', function() {
-  this.timeout(10000)
+  this.timeout(10000);
 
   beforeEach(function() {
     this.app = new Application({
       path: 'MYukkuriVoice-darwin-x64/MYukkuriVoice.app/Contents/MacOS/MYukkuriVoice'
     })
-    return this.app.start()
+    return this.app.start();
   });
 
   afterEach(function() {
     if (this.app && this.app.isRunning()) {
-      return this.app.stop()
+      return this.app.stop();
     }
   });
 
-  // test
-  it('open application window at startup', function() {
-    return this.app.client.getWindowCount().then(function (count) {
-      assert.equal(count, 1)
-    })
+  it('open main window at startup', function() {
+    return this.app.client
+      .getWindowCount().then(function(count) {
+        assert.equal(count, 1);
+      })
+      //.isVisible('#main_pane').then(function(isVisible) {
+      //  assert.ok(isVisible);
+      //})
+      //.isVisible('#settings_pane').then(function(isVisible) {
+      //  assert.ok(! isVisible);
+      //})
+      .getTitle().then(function(title) {
+        assert.equal(title, 'MYukkuriVoice');
+      })
   });
 
-  // menu
-  it('menu', function(done) {
-    //var menu = this.app.electron.remote.Menu.getApplicationMenu();
-    //assert.ok(true);
-    return done();
+  it('mainWindow input', function() {
+    return this.app.client
+      // encode
+      .setValue('#source', 'test')
+      .click('#encode')
+      .getValue('#encoded').then(function(encoded) {
+        assert.equal(encoded, "テ'_スト");
+      })
+      // clear
+      .click('#clear')
+      .getValue('#source').then(function(source) {
+        assert.equal(source, '');
+      })
+      .getValue('#encoded').then(function(encoded) {
+        assert.equal(encoded, '');
+      })
+      // play and record is enabled
+      .isEnabled('#play').then(function(isEnabled) {
+        assert.ok(! isEnabled);
+      })
+      .setValue('#source', 'test')
+      .isEnabled('#play').then(function(isEnabled) {
+        assert.ok(isEnabled);
+      })
+      .click('#clear')
+      .isEnabled('#play').then(function(isEnabled) {
+        assert.ok(! isEnabled);
+      })
+      .setValue('#encoded', "テ'_スト")
+      .isEnabled('#play').then(function(isEnabled) {
+        assert.ok(isEnabled);
+      })
   });
 
+  it('mainWindow voice config', function() {
+    var voiceConfigLength = 999;
+    return this.app.client
+      // filter
+      .elements('.voice-config-item').then(function(response) {
+        assert.ok(response.value.length > 0);
+      })
+      .setValue('#filter-text', 'xxxxxxxxxxxxxxxxxx')
+      .elements('.voice-config-item').then(function(response) {
+        assert.equal(response.value.length, 0);
+      })
+      .setValue('#filter-text', '')
+      .elements('.voice-config-item').then(function(response) {
+        assert.ok(response.value.length > 0);
+      })
+      // add config
+      .elements('.voice-config-item').then(function(response) {
+        voiceConfigLength = response.value.length;
+      })
+      .click('#plus')
+      .elements('.voice-config-item').then(function(response) {
+        assert.equal(response.value.length, voiceConfigLength + 1);
+      })
+  });
+
+  it('mainWindow play', function() {
+    return this.app.client
+      .setValue('#encoded', "テ'_スト")
+      .click('#play')
+      .waitForText('#duration', 2000)
+      .getValue('#duration').then(function(duration) {
+        assert.ok(duration != '');
+      })
+      .click('#stop')
+  });
+
+  it('mainWindow alwaysOnTop', function() {
+    var app = this.app;
+    return this.app.client
+      .getAttribute('#always-on-top-btn span.icon', 'class').then(function(classes) {
+        assert.ok(! classes.includes('active'));
+        app.browserWindow.isAlwaysOnTop().then(function(isAlwaysOnTop) {
+          assert.ok(! isAlwaysOnTop);
+        })
+      })
+      .click('#always-on-top-btn')
+      .getAttribute('#always-on-top-btn span.icon', 'class').then(function(classes) {
+        assert.ok(classes.includes('active'));
+        app.browserWindow.isAlwaysOnTop().then(function(isAlwaysOnTop) {
+          assert.ok(isAlwaysOnTop);
+        })
+      })
+      .click('#always-on-top-btn')
+      .getAttribute('#always-on-top-btn span.icon', 'class').then(function(classes) {
+        assert.ok(! classes.includes('active'));
+        app.browserWindow.isAlwaysOnTop().then(function(isAlwaysOnTop) {
+          assert.ok(! isAlwaysOnTop);
+        })
+      })
+  });
+
+  it('mainWindow help', function() {
+    return this.app.client
+      .click('#help')
+      .getWindowCount().then(function(count) {
+        assert.equal(count, 2);
+      })
+  });
+
+  it('mainWindow shortcut', function() {
+    return this.app.client
+      .isVisible('.introjs-tooltip').then(function(isVisible) {
+        assert.ok(! isVisible);
+      })
+      .click('#shortcut')
+      .isVisible('.introjs-tooltip').then(function(isVisible) {
+        assert.ok(isVisible);
+      })
+  });
+
+  it('mainWindow tutorial', function() {
+    return this.app.client
+      .isVisible('.introjs-tooltip').then(function(isVisible) {
+        assert.ok(! isVisible);
+      })
+      .click('#tutorial')
+      .isVisible('.introjs-tooltip').then(function(isVisible) {
+        assert.ok(isVisible);
+      })
+  });
+
+  it('mainWindow switchSettingsView', function() {
+    return this.app.client
+      .click('#switch-settings-view')
+      //.isVisible('#main_pane').then(function(isVisible) {
+      //  assert.ok(! isVisible);
+      //})
+      //.isVisible('#settings_pane').then(function(isVisible) {
+      //  assert.ok(isVisible);
+      //})
+  });
 
 });
 
