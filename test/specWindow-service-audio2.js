4cc612e82d60caabc68e8366cd69cb4fb58251c1
taku.omi <taku.omi@gree.co.jp>
2018-10-22 16:11:47 +0900

    update test code.

--- test/specWindow-service-audio2.js
+++ test/specWindow-service-audio2.js
@@ -2,13 +2,15 @@
 exports.__esModule = true;
 var spectron_1 = require("spectron");
 var assert = require("assert");
+var fs = require("fs");
 var temp = require("temp");
 temp.track();
 describe('specWindow-service-AudioService2', function () {
     this.timeout(10000);
+    var dirPath = null;
     before(function () {
         var fsprefix = "_myubo_test" + Date.now().toString(36);
-        var dirPath = temp.mkdirSync(fsprefix);
+        dirPath = temp.mkdirSync(fsprefix);
         this.app = new spectron_1.Application({
             path: 'MYukkuriVoice-darwin-x64/MYukkuriVoice.app/Contents/MacOS/MYukkuriVoice',
             env: { DEBUG: 1, NODE_ENV: 'test', userData: dirPath }
@@ -57,41 +59,58 @@ describe('specWindow-service-AudioService2', function () {
         })["catch"](function (err) {
             assert.fail(err.message);
         });
-        // TODO tmp file
-        // TODO file exists
     });
     it('record', function () {
         return this.client
             // record aquestalk1
             .setValue('#play2-encoded', "テ'_スト")
-            .setValue('#wav-file-path-2', '/tmp/_myukkurivoice_hogehoge.wav')
+            .setValue('#wav-file-path-2', dirPath + "/_myukkurivoice_hogehoge_1.wav")
             .setValue('#record-result-2', '')
             .click('#record2-aqver1')
             .waitForValue('#record-result-2', 5000)
             .getValue('#record-result-2').then(function (value) {
             assert.equal(value, 'ok');
+            return new Promise(function (resolve, reject) {
+                fs.readFile(dirPath + "/_myukkurivoice_hogehoge_1.wav", function (err, data) {
+                    assert.ok(!err);
+                    assert.ok(data);
+                    resolve();
+                });
+            });
         })
             // record aquestalk2
             .setValue('#play2-encoded', "テ'_スト")
-            .setValue('#wav-file-path-2', '/tmp/_myukkurivoice_hogehoge.wav')
+            .setValue('#wav-file-path-2', dirPath + "/_myukkurivoice_hogehoge_2.wav")
             .setValue('#record-result-2', '')
             .click('#record2-aqver2')
             .waitForValue('#record-result-2', 5000)
             .getValue('#record-result-2').then(function (value) {
             assert.equal(value, 'ok');
+            return new Promise(function (resolve, reject) {
+                fs.readFile(dirPath + "/_myukkurivoice_hogehoge_2.wav", function (err, data) {
+                    assert.ok(!err);
+                    assert.ok(data);
+                    resolve();
+                });
+            });
         })
             // record aquestalk10
             .setValue('#play2-encoded', "テ'_スト")
-            .setValue('#wav-file-path-2', '/tmp/_myukkurivoice_hogehoge.wav')
+            .setValue('#wav-file-path-2', dirPath + "/_myukkurivoice_hogehoge_10.wav")
             .setValue('#record-result-2', '')
             .click('#record2-aqver10')
             .waitForValue('#record-result-2', 5000)
             .getValue('#record-result-2').then(function (value) {
             assert.equal(value, 'ok');
+            return new Promise(function (resolve, reject) {
+                fs.readFile(dirPath + "/_myukkurivoice_hogehoge_10.wav", function (err, data) {
+                    assert.ok(!err);
+                    assert.ok(data);
+                    resolve();
+                });
+            });
         })["catch"](function (err) {
             assert.fail(err.message);
         });
-        // TODO tmp file
-        // TODO file exists
     });
 });
--- test/specWindow-service-audio2.ts
+++ test/specWindow-service-audio2.ts
@@ -1,14 +1,16 @@
 import {Application} from 'spectron';
 import * as assert from 'assert';
+import * as fs from 'fs';
 import * as temp from 'temp';
 temp.track();
 
 describe('specWindow-service-AudioService2', function() {
   this.timeout(10000);
 
+  let dirPath = null;
   before(function() {
     const fsprefix = `_myubo_test${Date.now().toString(36)}`;
-    const dirPath = temp.mkdirSync(fsprefix);
+    dirPath = temp.mkdirSync(fsprefix);
     this.app = new Application({
       path: 'MYukkuriVoice-darwin-x64/MYukkuriVoice.app/Contents/MacOS/MYukkuriVoice',
       env: {DEBUG: 1, NODE_ENV: 'test', userData: dirPath},
@@ -63,44 +65,61 @@ describe('specWindow-service-AudioService2', function() {
       .catch((err: Error) => {
         assert.fail(err.message);
       });
-      // TODO tmp file
-      // TODO file exists
   });
 
   it('record', function() {
     return this.client
       // record aquestalk1
       .setValue('#play2-encoded', "テ'_スト")
-      .setValue('#wav-file-path-2', '/tmp/_myukkurivoice_hogehoge.wav')
+      .setValue('#wav-file-path-2', `${dirPath}/_myukkurivoice_hogehoge_1.wav`)
       .setValue('#record-result-2', '')
       .click('#record2-aqver1')
       .waitForValue('#record-result-2', 5000)
       .getValue('#record-result-2').then((value: string) => {
         assert.equal(value, 'ok');
+        return new Promise((resolve, reject) => {
+          fs.readFile(`${dirPath}/_myukkurivoice_hogehoge_1.wav`, (err, data) => {
+            assert.ok(!err);
+            assert.ok(data);
+            resolve();
+          });
+        });
       })
       // record aquestalk2
       .setValue('#play2-encoded', "テ'_スト")
-      .setValue('#wav-file-path-2', '/tmp/_myukkurivoice_hogehoge.wav')
+      .setValue('#wav-file-path-2', `${dirPath}/_myukkurivoice_hogehoge_2.wav`)
       .setValue('#record-result-2', '')
       .click('#record2-aqver2')
       .waitForValue('#record-result-2', 5000)
       .getValue('#record-result-2').then((value: string) => {
         assert.equal(value, 'ok');
+        return new Promise((resolve, reject) => {
+          fs.readFile(`${dirPath}/_myukkurivoice_hogehoge_2.wav`, (err, data) => {
+            assert.ok(!err);
+            assert.ok(data);
+            resolve();
+          });
+        });
       })
       // record aquestalk10
       .setValue('#play2-encoded', "テ'_スト")
-      .setValue('#wav-file-path-2', '/tmp/_myukkurivoice_hogehoge.wav')
+      .setValue('#wav-file-path-2', `${dirPath}/_myukkurivoice_hogehoge_10.wav`)
       .setValue('#record-result-2', '')
       .click('#record2-aqver10')
       .waitForValue('#record-result-2', 5000)
       .getValue('#record-result-2').then((value: string) => {
         assert.equal(value, 'ok');
+        return new Promise((resolve, reject) => {
+          fs.readFile(`${dirPath}/_myukkurivoice_hogehoge_10.wav`, (err, data) => {
+            assert.ok(!err);
+            assert.ok(data);
+            resolve();
+          });
+        });
       })
       // catch error
       .catch((err: Error) => {
         assert.fail(err.message);
       });
-      // TODO tmp file
-      // TODO file exists
   });
 });
