c45edc7ba2ad2ee2b43f41388a4d734c2767de29
taku.omi <taku.omi@gree.co.jp>
2019-11-18 18:41:54 +0900

    update webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -20,35 +20,35 @@ module.exports = [
     },
     target: 'electron-main',
   },
-//  {
-//    entry: {
-//      main: './js/apps.main.js',
-//      dict: './js/apps.dict.js',
-//      help: './js/apps.help.js',
-//      helpsearch: './js/apps.helpsearch.js',
-//      system: './js/apps.system.js',
-//      spec: './js/apps.spec.js',
-//    },
-//    //mode: 'production',
-//    mode: 'development',
-//    output: {
-//      filename: 'renderer.[name].js',
-//      path: path.resolve(__dirname, 'dist'),
-//    },
-//    module: {
-//      rules: [
-//        //{
-//        //  test: /ctrl\.main\.js$/,
-//        //  use: {
-//        //    loader: 'script-loader',
-//        //  },
-//        //},
-//        {
-//          test: /\.js$/,
-//          exclude: path.resolve(__dirname, 'node_modules'),
-//        },
-//      ],
-//    },
-//    target: 'electron-renderer',
-//  },
+  {
+    entry: {
+      main: './js/apps.main.js',
+      dict: './js/apps.dict.js',
+      help: './js/apps.help.js',
+      helpsearch: './js/apps.helpsearch.js',
+      system: './js/apps.system.js',
+      spec: './js/apps.spec.js',
+    },
+    //mode: 'production',
+    mode: 'development',
+    output: {
+      filename: 'renderer.[name].js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        //{
+        //  test: /ctrl\.main\.js$/,
+        //  use: {
+        //    loader: 'script-loader',
+        //  },
+        //},
+        {
+          test: /\.js$/,
+          exclude: path.resolve(__dirname, 'node_modules'),
+        },
+      ],
+    },
+    target: 'electron-renderer',
+  },
 ];
