2a1cb21e4784a1be515a5a75c4cb1036b71b70c5
taku.omi <taku.omi@gree.co.jp>
2019-11-18 11:27:15 +0900

    update webpack config.

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+
+module.exports = [
+  {
+    entry: './electron.js',
+    //mode: 'production',
+    mode: 'development',
+    output: {
+      filename: 'app.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [{
+        test: /\.js$/,
+        exclude: path.resolve(__dirname, 'node_modules'),
+      }],
+    },
+    target: 'electron-main',
+  },
+  {
+    entry: {
+      main: './js/apps.main.js',
+      dict: './js/apps.dict.js',
+      help: './js/apps.help.js',
+      helpsearch: './js/apps.helpsearch.js',
+      system: './js/apps.system.js',
+      spec: './js/apps.spec.js',
+    },
+    //mode: 'production',
+    mode: 'development',
+    output: {
+      filename: 'renderer.[name].js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        //{
+        //  test: /ctrl\.main\.js$/,
+        //  use: {
+        //    loader: 'script-loader',
+        //  },
+        //},
+        {
+          test: /\.js$/,
+          exclude: path.resolve(__dirname, 'node_modules'),
+        },
+      ],
+    },
+    target: 'electron-renderer',
+  },
+];
